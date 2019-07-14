bb84f76eeaf7a53fd59e0715b0861da4f1dbccd1
lkito <lkiti17@freeuni.edu.ge>
2019-07-14 17:40:49 +0400

    added some more seeders

--- src/database/seeders/seeders_2_categories/seeder_4_item_category.sql
+++ src/database/seeders/seeders_2_categories/seeder_4_item_category.sql
@@ -9,7 +9,7 @@ VALUES  (1, 'prius', 1, 1),
         (6, 'baguette', 6, 6);
         (7, 'killer flavor', 7, 7),
         (8, 'coca-cola cherry', 8, 8),
-        (9, 'audi RS', 9, 9);
-        (10, 'chili flavored', 10, 10),
+        (9, 'audi RS', 1, 9);
+        (10, 'chili flavored', 9, 10),
         (11, 'iphone 23S', 4, 4),
         (12, 'iphone 23SSSSSSSSSSSSSSSSSSSSSS', 4, 4);
